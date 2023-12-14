39fd2213cbfb6d03a7b74a633c2a819300d8f74f
ddewaele <ddewaele@gmail.com>
2023-12-14 14:04:45 +0100

    Initial commit

---
+++ test/cdk-simple-ec2-private-rds.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkSimpleEc2PrivateRds from '../lib/cdk-simple-ec2-private-rds-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-simple-ec2-private-rds-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkSimpleEc2PrivateRds.CdkSimpleEc2PrivateRdsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
